2b7d648b8af1ac5860264c382fdbd9b13bfd4ca4
Maksymilian Bogun <mbogun@gmail.com>
2010-12-08 19:30:43 +0100

    zmiana nazwy tabelki

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -47,21 +47,21 @@ void MainWindow::on_pushButton_2_clicked()
     QString plik = QFileDialog::getOpenFileName(this, "Wybierz plik ograniczen", ".", "Pliki tekstowe (*.txt)");
     if(plik != "") {
         ograniczenia.Wczytaj(plik);
-        ui->tableWidget_2->setColumnCount(2);
-        ui->tableWidget_2->setRowCount(ograniczenia.ileParametrow());
+        ui->tabelkaOgraniczenia->setColumnCount(2);
+        ui->tabelkaOgraniczenia->setRowCount(ograniczenia.ileParametrow());
         ogr_dane.resize(ograniczenia.ileParametrow());
-        ui->tableWidget_2->setHorizontalHeaderLabels(QStringList() << "Parametr" << "Dopuszczalne wartosci");
-        ui->tableWidget_2->setColumnWidth(0,70);
-        ui->tableWidget_2->setColumnWidth(1,150);
+        ui->tabelkaOgraniczenia->setHorizontalHeaderLabels(QStringList() << "Parametr" << "Dopuszczalne wartosci");
+        ui->tabelkaOgraniczenia->setColumnWidth(0,70);
+        ui->tabelkaOgraniczenia->setColumnWidth(1,150);
         for(int i = 0; i < ograniczenia.ileParametrow(); ++i) {
             ogr_dane[i].first = QTableWidgetItem(QString::number(i));
-            ui->tableWidget_2->setItem(i, 0, &ogr_dane[i].first);
+            ui->tabelkaOgraniczenia->setItem(i, 0, &ogr_dane[i].first);
             QString tmp = QString("%1").arg(ograniczenia.Parametr(i,0));
             for(int j = 1; j < ograniczenia.ileOgraniczen(i); ++j) {
                 tmp.append(QString(", %1").arg(ograniczenia.Parametr(i,j)));
             }
             ogr_dane[i].second = QTableWidgetItem(tmp);
-            ui->tableWidget_2->setItem(i, 1, &ogr_dane[i].second);
+            ui->tabelkaOgraniczenia->setItem(i, 1, &ogr_dane[i].second);
         }
     }
 }
